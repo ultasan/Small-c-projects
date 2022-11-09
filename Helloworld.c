bf26241dcc94135acef3835ad80313adc573a14b
ultasan <93845673+ultasan@users.noreply.github.com>
2022-11-08 21:09:01 -0800

    Create Helloworld.c

---
+++ Helloworld.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s[100];  	
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    printf("Hello, World!\n");
+    printf("Welcome to C programming.");    
+    return 0;
+}
