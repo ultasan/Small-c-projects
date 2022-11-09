058104a3f8d4948137aaaaf8a78e7393d0852299
ultasan <93845673+ultasan@users.noreply.github.com>
2022-11-08 21:11:42 -0800

    Create subadd.c

---
+++ subadd.c
@@ -0,0 +1,11 @@
+int main()
+{   
+    int num1, num2;
+    float fly1, fly2;
+    scanf("%d %d", &num1, &num2);
+    printf("\n");
+    scanf("%f %f", &fly1, &fly2);
+    printf("%d %d\n", num1 + num2, num1 - num2);
+    printf("%0.1f %0.1f\n", fly1 + fly2, fly1 - fly2);
+    return 0;
+}
