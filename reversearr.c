1583a15d1889a4f155bf6f203ac34cdce6df8fbf
ultasan <93845673+ultasan@users.noreply.github.com>
2022-11-10 21:23:04 -0800

    Create reversearr.c

---
+++ reversearr.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr, i;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+    
+    for(num;num>0;num--){
+        printf("%d ", *(arr + num-1));
+    }
+    
+    return 0;
+}
