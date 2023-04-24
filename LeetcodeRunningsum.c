2c156651cdb28b522cd68b09b4541dde79090b3a
ultasan <93845673+ultasan@users.noreply.github.com>
2023-04-23 17:42:19 -0700

    Update LeetcodeRunningsum.c

--- LeetcodeRunningsum.c
+++ LeetcodeRunningsum.c
@@ -3,16 +3,12 @@
  */
 int* runningSum(int* nums, int numsSize, int* returnSize){
     int i;
-    int prevsum = 0;
     int currsum = 0;
     int* runningSum = malloc(sizeof(int)*numsSize);
     for(i=0;i<numsSize;i++){
-        currsum = prevsum+nums[i];
-        prevsum = currsum;
+        currsum = currsum+nums[i];
         runningSum[i] = currsum;
     }
-
     *returnSize = numsSize;
-
     return runningSum;
 }
