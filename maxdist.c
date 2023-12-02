1ba19d386281da5ae6f9b1cc7452d9654a48f44a
ultasan <93845673+ultasan@users.noreply.github.com>
2023-12-01 20:42:32 -0800

    Add files via upload

---
+++ maxdist.c
@@ -0,0 +1,34 @@
+# include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(){
+    int i =0;
+    int n;
+    int j = 0;
+    int max = 0;
+    int xtemp = 0;
+    int ytemp = 0;
+    FILE* input = fopen("maxdist.in","r");
+    fscanf(input,"%d\n",&n);
+    int xcor[n];
+    int ycor[n];
+    for(i=0;i<n;i++){
+        fscanf(input,"%d",&xcor[i]);
+    }
+    for(i=0;i<n;i++){
+        fscanf(input,"%d",&ycor[i]);
+    }
+    for(i=0;i<n;i++){
+        for(j=i;j<n;j++){
+            xtemp = (xcor[i]-xcor[j])*(xcor[i]-xcor[j]);
+            ytemp = (ycor[i] - ycor[j])*(ycor[i]-ycor[j]);
+            if(max< abs(xtemp+ytemp)){
+                max = xtemp + ytemp;
+            }
+        }
+    }
+    fclose(input);
+    printf("\n\nMax: %d\n\n", max);
+
+}
